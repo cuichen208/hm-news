b3384b5b55c3647234e96744963ea9ef0c0d4785
cuichen208 <2280628277@qq.com>
2020-07-01 16:09:26 +0800

    框架搭建完成

--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import { Button, Field } from 'vant'
 import 'amfe-flexible'
-// import Vant from 'vant'
-// import 'vant/lib/index.css'
+import './styles/common.less'
+import './styles/iconfont.less'
+import router from './router'
 
-// Vue.use(Vant)
 Vue.use(Button)
 Vue.use(Field)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: c => c(App)
+  render: c => c(App),
+  router
 }).$mount('#app')
